dcfd147e2d8ba18dabc1825e13d78f3b61a50491
ghagevaibhav <ghagevaibhav@gmail.com>
2025-01-09 14:51:10 +0530

    canvas components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import { useEffect } from 'react';
 // import './App.css'
 
-function App() {
+const arrowYPos = 200;
+const arrowLen = 50;
 
+function drawCircle(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number, startAngle: number, endAngle: number) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, startAngle, endAngle);
+  ctx.fillStyle = 'blue';
+  ctx.fill();
+}
+
+function drawArrow(ctx: CanvasRenderingContext2D, x: number) {
+  ctx.beginPath();
+  ctx.moveTo(x, arrowYPos)
+  ctx.lineTo(x + arrowLen, arrowYPos)
+  ctx.moveTo(x, arrowYPos)
+  ctx.lineTo(x + 15, arrowYPos - 10)
+  ctx.moveTo(x, arrowYPos)
+  ctx.lineTo(x + 15, arrowYPos + 10)
+  
+  ctx.strokeStyle = '#000'
+  ctx.lineWidth = 2
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function App() {
+  // circle on canvas
   useEffect(() => {
     const c = document.getElementById('mycanvas') as HTMLCanvasElement;
-    if (c) {
-      const ctx = c.getContext('2d');
-      ctx?.beginPath();
-      ctx?.arc(95, 50, 40, 0, 2 * Math.PI);
-      ctx?.stroke();
+    const ctx = c.getContext('2d');
+    if (ctx) {
+      drawCircle(ctx, 100, 200, 40, 0, 2 * Math.PI);
+      drawArrow(ctx, 700);
     }
   }, [])
 
@@ -24,15 +48,23 @@ function App() {
         </div>
         <div className='flex'>
           <div className='flex flex-row justify-center mt-4 mr-4'>
-            <button className='px-4 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600'>Hit</button>
+            <button onClick={handleHit} className='px-4 py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600'>Hit</button>
           </div>
-          <div className='flex flex-row justify-center mt-4'>
+          <div onClick={handleReset} className='flex flex-row justify-center mt-4'>
             <button className='px-4 py-2 rounded-md text-white bg-red-500 hover:bg-red-600'>Reset</button>
           </div>
         </div>
       </div>
     </>
   )
+}
+
+function handleHit() {
+  // Add bubble logic here
+}
+
+function handleReset(){
+  
 }
 
 export default App
